034e55d7434f282588079f5d7bec5e0d93f6162e
Ajit George <ageorge@drwholdings.com>
2017-01-12 12:48:45 -0600

    simplify CanBeMadeLocal

--- src/main/java/intellijbugs/CanBeMadeLocal.java
+++ src/main/java/intellijbugs/CanBeMadeLocal.java
@@ -1,20 +1,18 @@
 package intellijbugs;
 
-import java.util.function.Consumer;
-
 public class CanBeMadeLocal {
-    private final Consumer<String> a;  // should warn that 'a' can be made local
-    private final Consumer<String> b;  // does warn that 'b' can be made local
+    private final String a;  // should warn that 'a' can be made local
+    private final String b;  // does warn that 'b' can be made local
 
     private CanBeMadeLocal() {
-        a = t -> {};
-        b = t -> {};
+        a = "a";
+        b = "b";
 
-        a.accept(null);
-        b.accept(null);
+        System.out.println(a);
+        System.out.println(b);
 
         // With these lines, IntelliJ does not recognize that 'a' can be made local.
-        Runnable runnable = () -> a.accept("");
+        Runnable runnable = () -> System.out.println(a);
         runnable.run();
     }
 
